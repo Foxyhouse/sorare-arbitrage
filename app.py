2e946c9b692f96b42bd773a976e0f26e8195c312
Foxyhouse <143750170+Foxyhouse@users.noreply.github.com>
2026-05-15 16:47:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_segmented_floors(player_slug, is_in_season, jwt_token):
     query = """
     query GetSegFloors($slug: String!) {
       tokens {
-        all_offers: liveSingleSaleOffers(playerSlug: $slug, first: 50) {
+        all_offers: liveSingleSaleOffers(playerSlug: $slug, first: 500) {
           nodes { 
             senderSide { anyCards { rarityTyped seasonYear } }
             receiverSide { amounts { eurCents } } 
